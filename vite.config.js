db83a341e12106c09842d959733672d39213566a
titainis <titukas0228@gmail.com>
2026-07-07 16:16:56 +0300

    renaming

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(),
   ],
-  base: "/project-repo/",
+  base: "/movie-page/",
   css: {
   preprocessorOptions: {
     scss: {
